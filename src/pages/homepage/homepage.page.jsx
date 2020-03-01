cf8752be1feee8841413d877ad7e502d0f73eda2
ttcode10 <ttcode10@gmail.com>
2020-03-01 12:53:40 +0800

    Added local match simulation feature

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import reqwest from 'reqwest';
 
-import { Row, Col, Checkbox, Radio, Table, Menu, Dropdown, Badge } from 'antd';
+import { Row, Col, Checkbox, Radio, Table, Menu, Dropdown, Badge, message } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
-// import WorldCupTable from './../../components/table/table.component';
 
 const Homepage = () => {
   const [tableState, setTableState] = useState({
@@ -31,32 +30,68 @@ const Homepage = () => {
 
   useEffect(() => {
     fetchData();
-    console.log('render')
   }, []);
 
-  const menu = (
-    <Menu>
-      <Menu.Item key="0">
-        <a href="http://www.alipay.com/">主队进球+1</a>
-      </Menu.Item>
-      <Menu.Item key="1">
-        <a href="http://www.taobao.com/">主队红牌+1</a>
-      </Menu.Item>
-      <Menu.Item key="2">
-        <a href="http://www.taobao.com/">主队黄牌+1</a>
-      </Menu.Item>
-      <Menu.Divider />
-      <Menu.Item key="3">
-        <a href="http://www.alipay.com/">客队进球+1</a>
-      </Menu.Item>
-      <Menu.Item key="4">
-        <a href="http://www.taobao.com/">客队红牌+1</a>
-      </Menu.Item>
-      <Menu.Item key="5">
-        <a href="http://www.taobao.com/">客队黄牌+1</a>
-      </Menu.Item>
-    </Menu>
-  );
+  const onRedChange = checked => {
+    setTableState({
+      ...tableState,
+      showRed: checked
+    })
+  }
+
+  const onYellowChange = checked => {
+    setTableState({
+      ...tableState,
+      showYellow: checked
+    })
+  }
+
+  const onLanguageChange = value => {
+    setTableState({
+      ...tableState,
+      language: value
+    });
+  }
+
+  const handleOperations = (type, matchId) => {
+    const foundIndex = tableState.data.findIndex(record => record.matchId === matchId);
+    const currentMatchHome = tableState.data[foundIndex].home[tableState.language];
+    const currentMatchGuest = tableState.data[foundIndex].guest[tableState.language]
+
+    if (foundIndex !== -1) {
+      const matchToChange = tableState.data[foundIndex];
+      let msg ='';
+      switch (type) {
+        case 'homeScore':
+          matchToChange.homeScore++;
+          msg = <span><b style={{color: 'red'}}>{currentMatchHome} {matchToChange.homeScore}</b> -  {matchToChange.guestScore} {currentMatchGuest}</span>
+          message.success(msg, 5);
+          break;
+        case 'homeRed':
+          matchToChange.homeRed++;
+          break;
+        case 'homeYellow':
+          matchToChange.homeYellow++;
+          break;
+        case 'guestScore':
+          matchToChange.guestScore++;
+          msg = <span>{currentMatchHome} {matchToChange.homeScore} -  <b style={{color: 'red'}}>{matchToChange.guestScore} {currentMatchGuest}</b></span>
+          message.success(msg, 5);
+          break;
+        case 'guestRed':
+          matchToChange.guestRed++;
+          break;
+        case 'guestYellow':
+          matchToChange.guestYellow++;
+          break;
+      }
+
+      setTableState({
+        ...tableState,
+        data: tableState.data
+      })
+    }
+  };
 
   const columns = [
     {
@@ -108,37 +143,36 @@ const Homepage = () => {
     {
       title: '本地模拟',
       dataIndex: 'operation',
-      render: () => (
-        <Dropdown overlay={menu} trigger={['click']}>
-          <a onClick={e => e.preventDefault()}>
-            模拟 <DownOutlined />
-          </a>
+      render: (text, record) => (
+        <Dropdown overlay={
+          <Menu>
+            <Menu.Item key="0">
+              <a onClick={e => handleOperations('homeScore', record.matchId)}>主队进球+1</a>
+            </Menu.Item>
+            <Menu.Item key="1">
+              <a onClick={e => handleOperations('homeRed', record.matchId)}>主队红牌+1</a>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <a onClick={e => handleOperations('homeYellow', record.matchId)}>主队黄牌+1</a>
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item key="3">
+              <a onClick={e => handleOperations('guestScore', record.matchId)}>客队进球+1</a>
+            </Menu.Item>
+            <Menu.Item key="4">
+              <a onClick={e => handleOperations('guestRed', record.matchId)}>客队红牌+1</a>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <a onClick={e => handleOperations('guestYellow', record.matchId)}>客队黄牌+1</a>
+            </Menu.Item>
+          </Menu>
+        } trigger={['click']}>
+          <a>模拟 <DownOutlined /></a>
         </Dropdown>
       ),
       width: '5%',
     }
   ];
-
-  const onRedChange = checked => {
-    setTableState({
-      ...tableState,
-      showRed: checked
-    })
-  }
-
-  const onYellowChange = checked => {
-    setTableState({
-      ...tableState,
-      showYellow: checked
-    })
-  }
-
-  const onLanguageChange = value => {
-    setTableState({
-      ...tableState,
-      language: value
-    });
-  }
 
   return (
     <div>
